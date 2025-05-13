01448763ec1c03c73edc784202387cb97ceb4f71
laugonzalezo <jnmogollon39@ucatolica.edu.co>
2025-05-12 22:34:21 -0500

    load img

--- index.js
+++ index.js
@@ -10,3 +10,23 @@ const server = http.createServer((req, res) => {
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
 });
+
+const axios = require('axios');
+const fs = require('fs');
+
+const imageUrl = 'https://webappstacc.blob.core.windows.net/webappblob/Ucatolica.png';
+
+async function downloadImage() {
+  try {
+    const response = await axios.get(imageUrl, { responseType: 'stream' });
+
+    // Guarda la imagen localmente
+    response.data.pipe(fs.createWriteStream('logo-descargado.png'));
+
+    console.log('Imagen descargada con éxito.');
+  } catch (error) {
+    console.error('Error al descargar la imagen:', error.message);
+  }
+}
+
+downloadImage();
