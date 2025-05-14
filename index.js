5ca81c34a474f70622039dbb1f4c5dfaec3f098d
laugonzalezo <jnmogollon39@ucatolica.edu.co>
2025-05-13 23:01:03 -0500

    add img

--- index.js
+++ index.js
@@ -11,3 +11,18 @@ server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
 });
 
+app.get('/', (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="es">
+    <head>
+      <meta charset="UTF-8" />
+      <title>Mi app</title>
+    </head>
+    <body>
+      <h1>Bienvenido</h1>
+      <img src="https://webappstacc.blob.core.windows.net/webappblob/Ucatolica.png" alt="Ucatolica" />
+    </body>
+    </html>
+  `);
+});
