54a1551c79e09eab5786599fc32dde014f935f52
laugonzalezo <jnmogollon39@ucatolica.edu.co>
2025-05-13 22:49:54 -0500

    add img

--- index.js
+++ index.js
@@ -11,22 +11,13 @@ server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
 });
 
-const axios = require('axios');
-const fs = require('fs');
-
-const imageUrl = 'https://webappstacc.blob.core.windows.net/webappblob/Ucatolica.png';
-
-async function downloadImage() {
-  try {
-    const response = await axios.get(imageUrl, { responseType: 'stream' });
-
-    // Guarda la imagen localmente
-    response.data.pipe(fs.createWriteStream('logo-descargado.png'));
-
-    console.log('Imagen descargada con éxito.');
-  } catch (error) {
-    console.error('Error al descargar la imagen:', error.message);
-  }
-}
-
-downloadImage();
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <body>
+        <h1>Mi imagen desde Blob Storage</h1>
+        <img src="https://webappstacc.blob.core.windows.net/webappblob/Ucatolica.png" />
+      </body>
+    </html>
+  `);
+});
