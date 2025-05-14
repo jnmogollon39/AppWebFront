894d285052929c634e8e75f7e87e9dd65c558f3b
laugonzalezo <jnmogollon39@ucatolica.edu.co>
2025-05-13 23:13:59 -0500

    only img

--- index.js
+++ index.js
@@ -3,16 +3,6 @@ const app = express();
 const http = require('http');
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Node prueba de modificación de despliegue 1!\n'
-  res.end(msg);
-});
-
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
-});
-
 app.get('/', (req, res) => {
   res.send(`
     <!DOCTYPE html>
@@ -24,4 +14,10 @@ app.get('/', (req, res) => {
     </body>
     </html>
   `);
+});
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);
 });
