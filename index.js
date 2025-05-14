dc4b162c60552155fb149a63a24b7448dd161640
laugonzalezo <jnmogollon39@ucatolica.edu.co>
2025-05-13 23:10:00 -0500

    add all

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  const msg = 'Hello Node prueba de modificación de despliegue 1!\n'
+  res.end(msg);
+});
+
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);
+});
 
 app.get('/', (req, res) => {
   res.send(`
     <!DOCTYPE html>
-    <html lang="es">
-    <head>
-      <meta charset="UTF-8" />
-      <title>Mi app</title>
-    </head>
+    <html>
+    <head><title>Imagen desde Blob Storage</title></head>
     <body>
-      <h1>Bienvenido</h1>
-      <img src="https://webappstacc.blob.core.windows.net/webappblob/Ucatolica.png" alt="Ucatolica" />
+      <h1>Mi imagen</h1>
+      <img src="https://webappstacc.blob.core.windows.net/webappblob/Ucatolica.png" alt="Ucatolica" width="400" />
     </body>
     </html>
   `);
