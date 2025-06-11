de9d376058d34e09ac1bd55afcd93e799ce5d1db
laugonzalezo <jnmogollon39@ucatolica.edu.co>
2025-06-11 16:11:34 -0500

    from_begining

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
-// src/FormComponent.jsx
-import { useState } from "react";
+const http = require('http');
+const port = ProcessingInstruction.env.Port || 3000;
 
-export default function FormComponent() {
-  const [name, setName] = useState("");
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    await fetch("https://intgrfunction.azurewebsites.net/", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ name }),
-    });
-    alert("Enviado al backend");
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>Nombre:</label>
-      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-      <button type="submit">Enviar</button>
-    </form>
-  );
-}
+const server = http.createServer((req, res) => {More actions
+  res.statusCode = 200;
+  const msg = 'Hello Node!\n'
+  const msg = 'Hello Node prueba de modificación de despliegue 1!\n'
+  res.end(msg);
+});
