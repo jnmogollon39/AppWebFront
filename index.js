dd8b8a2cfa95d36734144accf585ea3293f74a84
laugonzalezo <jnmogollon39@ucatolica.edu.co>
2025-06-11 16:44:10 -0500

    new structure front

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-const http = require('http');
-const port = ProcessingInstruction.env.Port || 3000;
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const server = http.createServer((req, res) => {More actions
-  res.statusCode = 200;
-  const msg = 'Hello Node!\n'
-  const msg = 'Hello Node prueba de modificacion de despliegue 1!\n'
-  res.end(msg);
-});
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
