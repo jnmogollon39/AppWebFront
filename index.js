4b068bb781d43434a2ab670fbe5ac56ed1a6cb80
laugonzalezo <jnmogollon39@ucatolica.edu.co>
2025-06-11 16:23:38 -0500

    de nuevo

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const port = ProcessingInstruction.env.Port || 3000;
 const server = http.createServer((req, res) => {More actions
   res.statusCode = 200;
   const msg = 'Hello Node!\n'
-  const msg = 'Hello Node prueba de modificación de despliegue 1!\n'
+  const msg = 'Hello Node prueba de modificacion de despliegue 1!\n'
   res.end(msg);
 });
